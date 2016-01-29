45bd0749a5e3ce4e05fe33fc222fb1ac0e462031
Dmitrii Krasnov <dmitrii.krasnov@izi.travel>
2016-01-29 13:52:50 +0300

    SRV-7735: updated config

--- config/config.js
+++ config/config.js
@@ -27,53 +27,53 @@ module.exports = {
     geocoding_commands: {
       title: 'Гео-кодинг',
       commands: {
-        // cities_update_museums: {
-        //   title: 'Update museums for cities',
-        //   description: 'Обновляет привязку музеев к городам - забирает из google данные о городе для тех музеев, у которых нет этих данных. Запускается в 6:00 UTC ежедневно.',
-        //   cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:update:museums"
-        // },
-
-        // cities_update_tours: {
-        //   title: 'Update tours for cities',
-        //   description: 'Обновляет привязку туров к городам - забирает из google данные о городе для тех туров, у которых нет этих данных. Запускается в 6:00 UTC ежедневно.',
-        //   cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:update:tours"
-        // },
         cities_geocode_one: {
           title: 'Geocode one StorySet',
           description: 'Делает реверс геокодинг с обновление привязки для одного конкретного музея или тура по id.',
-          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:geocode_one[{0}]",
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake geocode:one[{0}]",
           params: [
             {
               default: '',
               title: 'StorySet id'
             }
           ]
-        }
-      }
-    },
-
-    cities_and_countries_commands: {
-      title: 'Города и страны',
-      commands: {
-        cities_story_set_calculate: {
-          title: 'Calculate storysets for cities',
-          description: 'Наполняет индекс elasticsearch данными о наличии и количестве контента в стране и городе с учетом языка и ключа API. Max execution time 5 min. Запускается раз в пол часа.',
-          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:story_set_calculate"
         },
         cities_locate: {
           title: 'Locate cities',
           description: 'По названию идет в google и получает location и bounds для города. Запускается в 6:00 UTC ежедневно.',
-          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:locate"
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:geocode"
         },
         countries_locate: {
           title: 'Locate countries',
           description: 'По названию идет в google и получает location и bounds для страны. Запускается в 6:00 UTC ежедневно.',
-          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake countries:locate"
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake countries:geocode"
         },
+        tours_locate: {
+          title: 'Reverse geocode tours',
+          description: 'Привязывает туры/квесты к городам',
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake tours:geocode"
+        },
+        museums_locate: {
+          title: 'Reverse geocode museums',
+          description: 'Привязывает музеи к городам',
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake museums:geocode"
+        }
+      }
+    },
+
+
+    cities_and_countries_commands: {
+      title: 'Города и страны',
+      commands: {
         cities_translate: {
           title: 'Translate cities',
           description: 'Забирает все переводы для городов из википедии. Запускается раз в час.',
           cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:translate"
+        },
+        cities_story_set_calculate: {
+          title: 'Calculate storysets for cities',
+          description: 'Наполняет индекс elasticsearch данными о наличии и количестве контента в стране и городе с учетом языка и ключа API. Max execution time 5 min. Запускается раз в пол часа.',
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cities:story_set_calculate"
         }
       }
     },
@@ -104,35 +104,23 @@ module.exports = {
         }
       }
     },
-
-    izi_travel: {
-      title: 'izi.TRAVEL',
-      commands: {
-        update_all_data: {
-          title: 'Обновить список городов и стран',
-          description: 'Получает данные по городам и странам из API. При успешном выполнении удаляет существующие данные и заменяет их новыми. Запускается каждый час в 6 и 36 минут.',
-          cmd: "cd /home/deployer/apps/izi-travel/current && IZI_CONFIG=/home/deployer/apps/izi-travel/shared/config/izi_config.yml RAILS_ENV=production bundle exec rake db:prepare"
-        },
-        clear_travel_cache: {
-          title: 'Очистить кэш на izi.TRAVEL',
-          description: 'Чистит все закешированные записи и Travel начинает общаться с API',
-          cmd: "cd /home/deployer/apps/izi-travel/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake cache:clear"
-        }
-      }
-    },
-
     rest_commands: {
       title: 'Остальное',
       commands: {
         statistics_reindex: {
           title: 'Reindex statistics',
           description: 'Забирает статисику с гугла и кладет в идекс elasticsearch. Max execution time about 5 min. Запускается в 6:00 UTC ежедневно.',
-          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake statistics:reindex"
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake stats:reindex"
         },
         update_reviews_average: {
           title: 'Обновить сводку отзывов и оценок для API.',
           description: 'Собрает количество отзывов и среднюю оценку для каждого MTGObject\'а. Запускается в 6:00 UTC ежедневно.',
-          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake reviews:rating:all && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake reviews:rating"
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake ratings:aggregate"
+        }, 
+        quest_validation: {
+          title: 'Переодическая валидация квестов',
+          description: 'Проверяет квесты на валидность, в случае не соответсвия - добавляет в админку соответствующие записи и шлёт письма модератору. Запускается в 8:00 UTC ежедневно.',
+          cmd: "cd /home/deployer/apps/izi-cms/current && IZI_CONFIG=config/izi_config.yml RAILS_ENV=production bundle exec rake consistency:validate_quests"
         }
       }
     }
